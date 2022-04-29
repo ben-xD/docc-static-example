578991b87a0eda8e39926060bb0b0282991437ab
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2022-04-29 15:21:40 +0100

    chore: update script to build for github pages

--- build.sh
+++ build.sh
@@ -6,17 +6,24 @@ set -euxo pipefail
 
 TARGET_NAME=SlothCreator
 OUTPUT_DIR=docs
-# swift package --allow-writing-to-directory $OUTPUT_DIR \ 
+git clean -fx build
+# Build for root path: eg http://localhost:8000/documentation/SlothCreator
+# This is useful if you have a domain/subdomain specifically for this documentation
+# swift package --allow-writing-to-directory $OUTPUT_DIR \
 #     generate-documentation \
 #     --target $TARGET_NAME \
 #     --disable-indexing \
 #     --output-path $OUTPUT_DIR \
 #     --transform-for-static-hosting
 
-git clean -fx build
+
+# Build for specified path: eg http://localhost:8000/docc-static-example/documentation/SlothCreator
+# This is useful for github pages
+WEB_PATH=/docc-static-example
 swift package --allow-writing-to-directory $OUTPUT_DIR \
     generate-documentation \
     --target $TARGET_NAME \
     --disable-indexing \
     --output-path $OUTPUT_DIR \
-    --transform-for-static-hosting
+    --transform-for-static-hosting \
+    --hosting-base-path $WEB_PATH
